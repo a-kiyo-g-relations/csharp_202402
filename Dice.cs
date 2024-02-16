856a71c78f9d41419cfaf49976706dc0d6fe54fb
TSUBASA\t-nag <t-nagatsuka@g-relations.jp>
2024-02-16 17:40:57 +0900

    #98 ダイスクラスの作成 定数名の変更 メンバ変数名の変更 乱数保持のための配列を定義

--- Dice.cs
+++ Dice.cs
@@ -2,39 +2,46 @@
 
 public class Dice
 {
-    //サイコロの出目に出力する値   
-    private const int DicePipValue1 = 1;
-    //サイコロの出目に出力する値 
-    private const int DicePipValue2 = 2;
-    //サイコロの出目に出力する値 
-    private const int DicePipValue3 = 3;
-    //サイコロの出目に出力する値 
-    private const int DicePipValue4 = 4;
-    //サイコロの出目に出力する値 
-    private const int DicePipValue5 = 5;
-    //サイコロの出目に出力する値 
-    private const int DicePipValue6 = 6;
+    // サイコロの出目に出力する値   
+    private const int DicePip1 = 1;
+    private const int DicePip2 = 2;
+    private const int DicePip3 = 3;
+    private const int DicePip4 = 4;
+    private const int DicePip5 = 5;
+    private const int DicePip6 = 6;
 
-    //乱数を入れるための配列
-    private int[] numbers;
-    
-    //ランダマイザ
+    // サイコロの出目の定数を入れるための配列
+    private int[] pipDice = new int[] { 
+        DicePip1, DicePip2, DicePip3, 
+        DicePip4, DicePip5, DicePip6 };
+
+    //サイコロの乱数を保持するための配列
+    private int[] diceNumbers = new int[3];
+
+    // ランダマイザ
     private Random random;
 
     // コンストラクタ
     public Dice()
     {
-        numbers = new int[] {
-            DicePipValue1, DicePipValue2, DicePipValue3,
-            DicePipValue4, DicePipValue5, DicePipValue6};
         random = new Random();
     }
 
-    public Random Random { get => random; set => random = value; }
-
+    // 乱数を生成する
     public int GetRandomDiceNumber()
     {
-        int randomIndex = random.Next(numbers.Length);
-        return numbers[randomIndex];
+        int randomIndex = random.Next(pipDice.Length);
+        return pipDice[randomIndex];
+    }
+
+    // 乱数を指定の数ループし格納
+    public int[] GenerateRandomNumbers()
+    {
+        int count = 3; // 生成する乱数の数
+        for (int i = 0; i < count; i++)
+        {
+            diceNumbers[i] = GetRandomDiceNumber();
+        }
+        return diceNumbers;
     }
 }
