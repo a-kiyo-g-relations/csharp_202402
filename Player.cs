0a8a69035a06842e841cc5105bdabab73c2d342d
TSUBASA\t-nag <t-nagatsuka@g-relations.jp>
2024-02-21 17:50:38 +0900

    #99 プレイヤークラスの作成 ・メソッド名などの見直し ・インデント修正 ・行末コメントの削除

--- Player.cs
+++ Player.cs
@@ -4,19 +4,19 @@ using System.Linq;
 public class Player
 {
     // サイコロの出目から役を判定するための値   
-    public enum RoleDecision
+    public enum DetermineValue
     {
         One = 1,
         Two = 2
     }
 
+    Dice dice = new Dice();
+
     // サイコロの乱数を保持するための配列
     private int[] diceValue = new int[3];
 
-    Dice dice = new Dice();
-
     // 指定の数ループし、取得した値を保持
-    public void GenerateRandomNumbers()
+    public void LoopCountNumber()
     {
         int count = 3;
         for (int i = 0; i < count; i++)
@@ -33,13 +33,13 @@ public class Player
     }
 
     // サイコロの出目の役を判定する
-    public int EvaluateHand(int[] numbers)
+    public int DetermineDicPip(int[] numbers)
     {
-        if (numbers.Distinct().Count() == (int)RoleDecision.One)
+        if (numbers.Distinct().Count() == (int)DetermineValue.One)
         {
             return 2;
         }
-        else if (numbers.Distinct().Count() == (int)RoleDecision.Two)
+        else if (numbers.Distinct().Count() == (int)DetermineValue.Two)
         {
             return 1;
         }
