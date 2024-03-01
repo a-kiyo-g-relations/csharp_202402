c636197ebf0088dff7542e2317def8560feb8e39
TSUBASA\t-nag <t-nagatsuka@g-relations.jp>
2024-03-01 17:05:20 +0900

    #101 【課題】チンチロリンの拡張-3回までサイコロを振れるようにする

--- OutputWord.cs
+++ OutputWord.cs
@@ -10,27 +10,72 @@ public class OutputWord
         Console.ReadKey();
     }
 
-    // プレイヤーの順番であることを表示する
-    public static void ShowPlayerTurn()
+    // プレイヤーか判断して順番を表示する
+    public static void ShowPlayerTurn(bool isPlayer)
     {
-        Console.WriteLine("あなたの番です");
+        string playerName;
+        if (isPlayer)
+        {
+            playerName = "あなた";
+            Console.WriteLine($"{playerName}の番です");
+        }
+        else
+        {
+            playerName = "相手";
+            Console.WriteLine($"{playerName}の番です");
+        }
+    }
+
+
+    // Enterキーでサイコロを投げるを表示する
+    public static void ShowThrowDicePlayer()
+    {
         Console.WriteLine("Enterキーでサイコロを投げる");
         Console.ReadKey();
     }
 
-    // cpuの順番であることを表示する
-    public static void ShowCpuTurn()
+    // サイコロを投げますを表示する
+    public static void ShowThrowDiceCpu()
     {
-        Console.WriteLine("相手の番です");
         Console.WriteLine("サイコロを投げます");
     }
 
-    // サイコロの出目の表示する
-    public static void ShowPipDice(int[] numbers)
+    // 何回目のサイコロを投げるかを表示する
+    public static void ShowHowManyThrow(bool isPlayer, int number)
     {
+        string playerName;
+        if (isPlayer)
+        {
+            playerName = "あなた";
+            // trueだった場合あなたの順番であることを返す
+            Console.WriteLine($"{playerName}の{number}投目です");
+        }
+        else
+        {
+            playerName = "相手";
+            // falseだった場合相手の順番であることを返す
+            Console.WriteLine($"{playerName}の{number}投目です");
+        }
+    }
+
+    // プレイヤーのサイコロの出目の表示する
+    public static void ShowPipDicePlayer(int[] numbers)
+    {
+        Console.Write("あなた : ");
         foreach (int number in numbers)
         {
-            Console.Write($"[{number}]");
+            Console.Write($"[{number}] ");
+        }
+        Console.WriteLine();
+    }
+
+    // cpuのサイコロの出目の表示する
+    public static void ShowPipDiceCpu(int[] numbers)
+    {
+        Console.Write("相手　 : ");
+        foreach (int number in numbers)
+        {
+            Console.Write($"[{number}] ");
         }
         Console.WriteLine();
     }
@@ -67,11 +112,11 @@ public class OutputWord
     // 勝敗結果を表示する
     public static void ShowResult(Constants.ResultRoll result)
     {
-        if(result == Constants.ResultRoll.Win)
+        if (result == Constants.ResultRoll.Win)
         {
             Console.WriteLine("あなたの勝ちです");
         }
-        else if(result == Constants.ResultRoll.Lose)
+        else if (result == Constants.ResultRoll.Lose)
         {
             Console.WriteLine("あなたの負けです");
         }
